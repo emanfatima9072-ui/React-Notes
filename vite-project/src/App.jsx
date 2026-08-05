e9544051ce6270448d01fc47441af3daa4bc1ebb
emanfatima9072-ui <emanfatima9072@gmail.com>
2026-08-05 18:07:11 +0500

    Add Custom Project

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,10 +1,23 @@
 
+//------------for vite project--------------------//
+// function App() {
+//   return ( 
+//     <h1>Vite Project</h1>
+//   );
+// }
 
+// export default App
+//-------------------changes for custom project--------------------------//
+import Chai from "./chai"
 function App() {
-  return ( 
-    <h1>Vite Project</h1>
-  );
+  const username = "chai aur code"
+  return (
+    <>
+    <Chai/>
+    <h1>chai aur react {username}</h1>
+    <p>test para</p>
+   </>
+  )
 }
-
 export default App
- 
+
--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,10 +1,48 @@
+//--------------------for vite project--------------------------------//
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// import App from './App.jsx'
+
+//  const root =   ReactDOM.createRoot(document.getElementById('root'));
+//  root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+//-------------------changes for custom app-----------------------//
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 
- const root =   ReactDOM.createRoot(document.getElementById('root'));
- root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+//import App from './App.jsx'
+//-----------MyApp wale example-----------------------//
+// function MyApp(){
+//     return (
+//         <div>
+//             <h1>Custom App | chai</h1>
+//         </div>
+//     )
+// }
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//     <MyApp />
+// )
+//------------------React.createElement() example----------------------//
+const anotherElement = (
+    <a href="https://google.com" target="_blank">
+        Visit Google
+    </a>
+)
+
+const reactElement = React.createElement(
+    'a',
+    {
+        href: 'https://google.com',
+        target: '_blank'
+    },
+    'Click me to visit Google',
+    anotherElement
+)
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    reactElement
+)
+
